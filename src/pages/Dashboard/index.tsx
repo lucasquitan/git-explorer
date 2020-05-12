aca82719e041bcea2b9094afba7bd8eda2f31c69
Lucas Quintanilha <l.quitan@gmail.com>
2020-05-12 00:27:13 -0300

    🐛 bug fixes on Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories, Error } from './styles';
@@ -81,7 +82,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -91,8 +95,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight className="details" />
-            {/* <FiTrash2 className="delete" /> */}
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
